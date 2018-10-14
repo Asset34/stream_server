84fc18a0fc0ddcbcb5db5fd30c59d9c5425fe196
Asset34 <artemiy.kashintsev@gmail.com>
2018-10-14 19:58:37 +0300

    OutputBox: import class

---
+++ src/ui/widgets/outputbox.hpp
@@ -0,0 +1,34 @@
+#ifndef VALUEOUTPUTBOX_HPP
+#define VALUEOUTPUTBOX_HPP
+
+#include <QWidget>
+
+class QHBoxLayout;
+class QString;
+class QLabel;
+
+class OutputBox : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit OutputBox(const QString &name = QString(),
+                       const QString &output = QString(),
+                       QWidget *parent = nullptr);
+
+    QString getName() const;
+    void setName(const QString &name);
+
+public slots:
+    void setText(const QString &text);
+    void setValue(int value);
+    void setValue(double value);
+
+private:
+    QHBoxLayout *m_layout;
+    QLabel *m_nameLabel;
+    QLabel *m_outputLabel;
+
+};
+
+#endif // VALUEOUTPUTBOX_HPP
