8803e01f336d90467fe3bab750b31683b7782826
Asset34 <artemiy.kashintsev@gmail.com>
2018-10-21 19:38:06 +0300

    project: set correct 'main' function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <QApplication>
-#include <QDebug>
 
 #include <ui/windows/mainwindow.hpp>
 
@@ -7,8 +6,8 @@ int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-    MainWindow mw;
-    mw.show();
+    MainWindow window;
+    window.show();
 
     return app.exec();
 }
