12f818f069d2d57f39f386eeaeeafbdaebaa0cfa
Asset34 <artemiy.kashintsev@gmail.com>
2018-10-21 13:21:02 +0300

    TranscodeSubpanel: implement class

---
+++ src/ui/subpanels/transcodesubpanel.cpp
@@ -0,0 +1,50 @@
+#include "transcodesubpanel.hpp"
+
+#include <QVBoxLayout>
+#include <QCheckBox>
+
+#include <ui/subpanels/videosubpanel.hpp>
+#include <ui/subpanels/audiosubpanel.hpp>
+
+TranscodeSubpanel::TranscodeSubpanel(VlcManager *vlcManager,
+                                     QWidget *parent)
+    : QGroupBox(parent),
+      m_vlcManager(vlcManager)
+{
+    // Create enable check box
+    m_enableCheckBox = new QCheckBox;
+    m_enableCheckBox->setEnabled(true);
+
+    // Create video subpanel
+    m_videoSubpanel = new VideoSubpanel(vlcManager);
+
+    // Create audio subpanel
+    m_audioSubpanel = new AudioSubpanel(vlcManager);
+
+    // Create layout
+    m_layout = new QVBoxLayout;
+    m_layout->setContentsMargins(0, 0, 0, 0);
+
+    // Create widget
+    setTitle("Transcode");
+    setContentsMargins(5, 20, 5, 5);
+    setLayout(m_layout);
+
+    // Create connections
+    connect(m_enableCheckBox, &QCheckBox::clicked, m_videoSubpanel, &VideoSubpanel::setEnabled);
+    connect(m_enableCheckBox, &QCheckBox::clicked, m_audioSubpanel, &AudioSubpanel::setEnabled);
+}
+
+TranscodeSubpanel::~TranscodeSubpanel()
+{
+}
+
+void TranscodeSubpanel::setParameters()
+{
+    SoutBuilder &builder = m_vlcManager->getSoutBuilder();
+
+    builder.setTranscodeEnabled(m_enableCheckBox->isChecked());
+
+    m_videoSubpanel->setParameters();
+    m_audioSubpanel->setParameters();
+}
---
+++ src/ui/subpanels/transcodesubpanel.hpp
@@ -0,0 +1,37 @@
+#ifndef TRANSCODESUBPANEL_HPP
+#define TRANSCODESUBPANEL_HPP
+
+#include <QGroupBox>
+
+#include "vlcmanager.hpp"
+
+class QVBoxLayout;
+class QCheckBox;
+class VideoSubpanel;
+class AudioSubpanel;
+
+class TranscodeSubpanel : public QGroupBox
+{
+    Q_OBJECT
+
+public:
+    explicit TranscodeSubpanel(VlcManager *vlcManager,
+                               QWidget *parent = nullptr);
+    virtual ~TranscodeSubpanel();
+
+public slots:
+    void setParameters();
+
+private:
+    QVBoxLayout *m_layout;
+
+    QCheckBox *m_enableCheckBox;
+
+    VideoSubpanel *m_videoSubpanel;
+    AudioSubpanel *m_audioSubpanel;
+
+    VlcManager *m_vlcManager;
+
+};
+
+#endif // TRANSCODESUBPANEL_HPP
